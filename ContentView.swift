db7bc76b6e5e31c24f043c04aaf65babe70c2bbe
Tyler Davis <coding@tydavis.dev>
2024-04-27 23:14:00 -0700

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  Food Decider
+//
+//  Created by Tyler Davis on 4/27/24.
+//
+
+import SwiftUI
+
+var dinnerRestaurants = ["The Stand", "Wahoo's", "BJ's", "Board and Brew", "Moreno's","Chipotle","Shake Shack","Islands","Cheesecake Factory","In N Out","Chick Fil A","Blaze Pizza","Smashburger","MOD Pizza","California Pizza Kitchen","Flame Broiler","Raising Canes","Ruby's","Gramm00's","Manga Bene","Panda Express"]
+var lunchRestaurants = ["The Stand", "Wahoo's", "BJ's", "Board and Brew","Chipotle","Shake Shack","Islands","In N Out","Chick Fil A","Blaze Pizza","Smashburger","MOD Pizza","California Pizza Kitchen","Flame Broiler","Raising Canes","Ruby's"]
+var breakfastRestaurants = ["Bravo Burger", "Corky's","Latte Da","San Juan Hills Country Club","The Original Pancake House"]
+
+struct ContentView: View {
+    var allRestaurants = breakfastRestaurants + lunchRestaurants + dinnerRestaurants
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red, .yellow, .teal]
+    
+    private var defaultRestaurant:String = "The Stand"
+    @State private var selected:String = "The Stand"
+    @State private var id:Int = 1
+    @State private var meal:String = "all"
+    var body: some View {
+        VStack{
+            Text("Why not try…")
+                .font(.largeTitle.bold())
+            Spacer()
+            VStack{
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "fork.knife.circle.fill"))
+                    .font(.system(size: 250))
+                    .foregroundColor(.white)
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            .transition(.slide)
+            .id(id)
+            Button("Directions") {
+                withAnimation(.easeInOut(duration: 0.50)) {
+                    let url = URL(string: "maps://?q=\(selected)")
+                    if UIApplication.shared.canOpenURL(url!) {
+                          UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                    }
+                }
+            }
+            .buttonStyle(.bordered)
+            Spacer()
+            HStack{
+                Button("Breakfast") {
+                    withAnimation(.easeInOut(duration: 0.50)) {
+                        meal = "breakfast"
+                        selected = breakfastRestaurants.randomElement() ?? defaultRestaurant
+                        id += 1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Lunch") {
+                    withAnimation(.easeInOut(duration: 0.50)) {
+                        meal = "lunch"
+                        selected = lunchRestaurants.randomElement() ?? defaultRestaurant
+                        id += 1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Dinner") {
+                    withAnimation(.easeInOut(duration: 0.50)) {
+                        meal = "dinner"
+                        selected = dinnerRestaurants.randomElement() ?? defaultRestaurant
+                        id += 1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Any") {
+                    withAnimation(.easeInOut(duration: 0.50)) {
+                        meal = "all"
+                        selected = allRestaurants.randomElement() ?? defaultRestaurant
+                        id += 1
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food_DeciderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_DeciderApp.swift
+//  Food Decider
+//
+//  Created by Tyler Davis on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Food_DeciderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
